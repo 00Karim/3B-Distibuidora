30327d9ff407586fedf1eed652c28b5dd9ad8076
franbaudrix <franco39239@gmail.com>
2025-09-24 17:24:28 -0300

    Creacion de los metodos propios de categorias

--- backend/models/category.model.js
+++ backend/models/category.model.js
@@ -1,5 +1,58 @@
+const mongoose = require("mongoose")
+const Category = require("../models/entities/category")
+
 class CategoryModel{
-    
+    static getAllCategories = async() => {
+        try{
+            categories = await Category.find()
+
+            return categories
+        }catch(e){
+            throw new Error(`Error, no se pudo obtener las categorias, ${e}`)
+        }
+    }
+
+    static getCategoryById = async(categoryId) => {
+        try{
+            if(!mongoose.Types.ObjectId.isValid(categoryId)){
+                throw new Error("Error, el ID no es valido")
+            }
+
+            const category = await Category.findById(categoryId)
+
+            return category
+        }catch(e){
+            throw new Error(`Error, no se pudo obtener la categoria indicada por ID`)
+        }
+    }
+
+    static createCategory = async(categoryData) => {
+        try{
+            const category = new Category(categoryData)
+            category.save()
+            return category
+        }catch(e){
+            throw new Error(`Error, no se pudo crear una nueva categoria`)
+        }
+    }
+
+    static updateCategory = async(idCategory, categoryData) => {
+        try{
+           if(!mongoose.Types.ObjectId.isValid(idCategory)){
+                throw new Error("Error, el ID no es valido")
+            }
+            
+            category = await Category.findByIdAndUpdate(
+                idCategory,
+                categoryData,
+                {new: true, runValidators: true}
+            )
+
+            return category
+        }catch(e){
+            throw new Error(`Error, no se pudo actualizar la categoria indicada, ${e}`)
+        }
+    }
 }
 
 module.exports = CategoryModel 
