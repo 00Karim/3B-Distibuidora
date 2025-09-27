0545d888cab7c738f4c4f9c3a5decff42af9939a
franbaudrix <franco39239@gmail.com>
2025-09-27 18:00:53 -0300

    Creacion de service order

--- backend/models/item.model.js
+++ backend/models/item.model.js
@@ -1,57 +1,5 @@
-const Item = require("../models/entities/item")
-
 class Item {
-    static getAllItems = async() => {
-        try{
-            const items = await Item.find()
 
-            return items
-        }catch(e){
-            throw new Error(`Error, no se pudo obtener los items, ${e}`)
-        }
-    }
-
-    static getItem = async(itemId) => {
-        try{
-            if(!mongoose.Types.ObjectId.isValid(itemId)){
-                throw new Error(`Error, el ID del item es invalido`)
-            }
-
-            const item = await Item.findById(itemId).populate('product')
-            return item
-        }catch(e){
-            throw new Error(`Error, no se pudo obtener el item, ${e}`)
-        }
-    }
-
-    static createItem = async(itemData) => {
-        try{
-            const item = new Item(itemData)
-            await item.save()
-            return item
-        }catch(e){
-            throw new Error(`Error, no se pudo crear el item, ${e}`)
-        }
-    }
-
-    static updateItem = async(itemId, itemData) => {
-        try{
-            if(!mongoose.Types.ObjectId.isValid(itemId)){
-                throw new Error(`Error, el ID del item es invalido`)
-            }
-
-            const updatedItem = await Item.findByIdAndUpdate(
-                itemId,
-                itemData,
-                {new: true, runValidators: true}
-            ).populate('product')
-
-            return updatedItem
-                
-        }catch(e){
-            throw new Error(`Error, no se pudo actualizar el Item, ${e}`)
-        }
-    }
 }
 
 module.exports = Item
