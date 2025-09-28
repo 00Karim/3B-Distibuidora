08603e6e56a0ec09f3ca6fdebe5b223d6e15fd01
00Karim <166453756+00Karim@users.noreply.github.com>
2025-09-27 21:42:34 -0300

    Agregacion de los modelos que faltan a la rama testing (#18)
    * Creacion del metodo Item
    
    * Creacion del metodo User
    
    * Creacion del metodo Adress
    
    * Creacion del metodo Client
    
    * Creacion del modeloPackagingType
    
    * Creacion del modelo Order
    
    ---------
    
    Co-authored-by: franbaudrix <franco39239@gmail.com>
    Co-authored-by: franbaudrix <85464296+franbaudrix@users.noreply.github.com>

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -43,9 +43,8 @@ class productModel {
 
     static getProductById = async(productId) => {
         try {
-            if(!mongoose.Types.ObjectId.isValid(productId)){
-                throw new Error(`Error, Id invalido`)
-            }
+            if(!mongoose.Types.ObjectId.isValid(productId))
+                throw new Error(`Error, Id del producto invalido`)
 
             const product = await Product.findById(productId).populate('category')
             
