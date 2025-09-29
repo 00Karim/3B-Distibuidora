21f9c0580086753cd3f8dadfa5b259712f710e29
00Karim <karimveleza@gmail.com>
2025-09-29 17:49:54 -0300

    Cambios en el readme para entrega

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose")
 const User = require("../models/entities/user")
 
 class UserModel {
@@ -24,9 +25,6 @@ class UserModel {
 
         static createUser = async(userData) => {
             try{
-                if(!mongoose.Types.ObjectId.isValid(userId))
-                    throw new Error(`Error, el ID ingresado no es valido ${e}`)
-
                 const newUser = new User(userData)
                 await newUser.save()
 
